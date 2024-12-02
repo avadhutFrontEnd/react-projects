e30e55df2fb4da24e9999d55633d1052cafa9a94
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-12-02 22:32:13 +0530

    [Course: Mastering React 16 > 6-Routing(56m) ] [ Video: #2-Setup_mp4_1min_46sec ] - for more information look at course-notes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
-import Movies from "./components/movies";
+import NavBar from "./components/navbar";
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <div>
+        <NavBar />
+      </div>
     );
   }
 }
