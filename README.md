ce48b556c4d902504ad73dc8dc9259c662b8b77a
Avadhut Jagde <avadhutjagde555@gmail.com>
2025-01-20 19:46:11 +0530

    [Course: Mastering React 16 > 11-Advanced Topics (1h) ] [ Video: #14-Updating-the-Context_mp4_7min_06sec ] -

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Created `react-advanced`  React Project.
 
 # Commit message format : 
-[Course: Mastering React 16 > 11-Advanced Topics (1h) ] [ Video: #13-Context-in-Functional-Components_mp4_3min_33sec ] -  
+[Course: Mastering React 16 > 11-Advanced Topics (1h) ] [ Video: #14-Updating-the-Context_mp4_7min_06sec ] -  
 
 # my-github Account : 
 https://github.com/avadhutFrontEnd
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import MoviePage from "./context/MoviePage";
 import UserContext from "./context/userContext";
+import Login from "./context/login";
 
 class App extends Component {
-  state = { currentUser: { name: "Mosh" } };
+  handleLoggedIn = (username) => {
+    console.log("Getting the user: " + username);
+    const user = { name: "Mosh" };
+    this.setState({ currentUser: user });
+  };
+
+  state = { currentUser: null };
 
   render() {
     return (
-      <UserContext.Provider value={this.state.currentUser}>
+      <UserContext.Provider
+      // This is the object that we're passing down in our component tree using  "context" :            
+        value={{
+          currentUser: this.state.currentUser,
+          onLoggedIn: this.handleLoggedIn,
+        }}
+      >
         <div>
           <MoviePage />
+          <Login />
         </div>
       </UserContext.Provider>
     );
